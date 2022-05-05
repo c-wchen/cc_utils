dc66b7c4bc0be266dcdb5b70df0e78cec97c7033
cwangcing <cwangcing@gmail.com>
2022-05-05 22:25:56 +0800

    fix: __cplusplus兼容问题修复

--- include/point_array.h
+++ include/point_array.h
@@ -1,7 +1,14 @@
 #ifndef __POINT_ARRAY_H__
 #define __POINT_ARRAY_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 void* createPointArray(int size);
 void setPointValue(void *rowPtr, uint32_t index, void *value);
 void* getPointValue(void *rowPtr, uint32_t index);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
