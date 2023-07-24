2a0cfa51804484b6a6197b65234b14ad4274c4c3
wchen <48781700+c-wchen@users.noreply.github.com>
2023-07-24 14:17:47 +0800

    Create test_stub.cpp

---
+++ testcase/test_stub.cpp
@@ -0,0 +1,31 @@
+#inlcude "stub.h"
+
+static void
+test_stub_fn(void) {
+  printf("%s\n", __func__);
+}
+
+static void
+test_fn(void) {
+  printf("%s\n", __func__);
+}
+
+void 
+test_void_fn() {
+
+}
+
+int main() {
+  stub_s *m;
+  m = stub_init(STUB_LEN);
+
+  ret = stub_set(m, test_fn, test_stub_fn);
+  if (ret) {
+    return -1;
+  }
+  test_fn();
+  stub_reset(m);
+  test_fn();
+  stub_finalize(m);
+  return 0;  
+}
