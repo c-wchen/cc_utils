48046a660663f233fdeb650478d635f2298d0340
cwangcing <cwangcing@gmail.com>
2025-05-14 12:48:04 -0400

    fix: 解决编译问题

--- src/cli/cli_server.c
+++ src/cli/cli_server.c
@@ -1,18 +1,30 @@
+#define _GNU_SOURCE 
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <assert.h>
+
 
 #include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <poll.h>
+
 
 #include "cli/cli.h"
 
-#define LOG_ERROR(fmt, ...)   printf("[ERROR] "fmt "[%s:%d]\n", __VA_ARGS__, __func__, __LINE__)
-#define LOG_INFO(fmt, ...)    printf("[INFO] "fmt "[%s:%d]\n", __VA_ARGS__, __func__, __LINE__)
-#define LOG_WARN(fmt, ...)    printf("[WARN] "fmt "[%s:%d]\n", __VA_ARGS__, __func__, __LINE__)
-#define LOG_DEBUG(fmt, ...)   printf("[DEBUG] "fmt "[%s:%d]\n", __VA_ARGS__, __func__, __LINE__)
+void do_accept(int32_t socket_fd);
+void *msg_poll(void *arg);
+int32_t bind_and_listen(const char *socket_path, int32_t *fd);
+
+#define LOG_ERROR(fmt, ...)   printf("[ERROR] " fmt "[%s:%d]\n", ##__VA_ARGS__, __func__, __LINE__)
+#define LOG_INFO(fmt, ...)    printf("[INFO] " fmt "[%s:%d]\n", ##__VA_ARGS__, __func__, __LINE__)
+#define LOG_WARN(fmt, ...)    printf("[WARN] " fmt "[%s:%d]\n", ##__VA_ARGS__, __func__, __LINE__)
+#define LOG_DEBUG(fmt, ...)   printf("[DEBUG] " fmt "[%s:%d]\n", ##__VA_ARGS__, __func__, __LINE__)
 
 typedef struct cli_handler {
     pthread_t cli_thread;
@@ -22,12 +34,12 @@ typedef struct cli_handler {
 cli_handler_t *command_handler = NULL;
 
 typedef struct command {
-    char *subcommand;
-    char *help;
+    const char *subcommand;
+    const char *help;
     void (*handler)(int32_t argc, char **argv);
 } command_t;
 
-#define CMD_DEF(subcommand, help, handler) {.subcommand = subcommand, .help = help, .handler =handler}
+#define CMD_DEF(s, h, d) {.subcommand = s, .help = h, .handler = d}
 
 
 void cmd_test(int32_t argc, char **argv)
@@ -36,11 +48,11 @@ void cmd_test(int32_t argc, char **argv)
     for (int32_t i = 0; i < argc; i++) {
         LOG_INFO("argv[%d] = %s", i, argv[i]);
     }
-    return 0;
+    return;
 }
 
-command_t comamnds[1024] = {
-    CMD_DEF("test", "test command", cmd_test);
+command_t commands[1024] = {
+    CMD_DEF("test", "test command", cmd_test)
 };
 
 
@@ -49,23 +61,23 @@ int cli_create(const char *name)
 {
     cli_handler_t *handler = (cli_handler_t *) malloc(sizeof(cli_handler_t));
     if (handler) {
-        return -NSPACE;
+        return -ENOSPC;
     }
     pid_t pid = getpid();
     char socket_path[64];
     sprintf(socket_path, "/var/tmp/%s.%d", name, pid);
-    int ret = bind_and_listen(socket_path, handler->socket_fd);
+    int ret = bind_and_listen(socket_path, &handler->socket_fd);
     if (ret < 0) {
         free(handler);
         return ret;
     }
-    int ret = pthread_create(&handler->thread, NULL, msg_poll, handler);
+    ret = pthread_create(&handler->cli_thread, NULL, msg_poll, handler);
 
     if (ret < 0) {
         LOG_ERROR("pthread create faild (%s).", strerror(ret));
         close(handler->socket_fd);
         free(handler);
-        return;
+        return ret;
     }
     command_handler = handler;
     return 0;
@@ -90,41 +102,41 @@ int32_t bind_and_listen(const char *socket_path, int32_t *fd)
     }
     int socket_fd = socket(PF_UNIX, SOCK_STREAM, 0);
 
-    if (sock_fd < 0) {
+    if (socket_fd < 0) {
         ret = errno;
-        LOG_ERROR("failed to create socket: %s", strerror(err));
-        return errno;
+        LOG_ERROR("failed to create socket: %s", strerror(ret));
+        return ret;
     }
 
     memset(&address, 0, sizeof(struct sockaddr_un));
     address.sun_family = AF_UNIX;
     snprintf(address.sun_path, sizeof(address.sun_path), "%s", socket_path);
 
-    if (bind(sock_fd, (struct sockaddr *)&address, sizeof(struct sockaddr_un)) != 0) {
+    if (bind(socket_fd, (struct sockaddr *)&address, sizeof(struct sockaddr_un)) != 0) {
         ret = errno;
         if (ret == EADDRINUSE) {
             // The old UNIX domain socket must still be there.
             // Let's unlink it and try again.
-            if (bind(sock_fd, (struct sockaddr *)&address, sizeof(struct sockaddr_un)) == 0) {
+            if (bind(socket_fd, (struct sockaddr *)&address, sizeof(struct sockaddr_un)) == 0) {
                 ret = 0;
             } else {
                 ret = errno;
             }
         }
         if (ret != 0) {
-            LOG_ERROR("failed to bind the UNIX domain socket to %s, ret: %s", sock_path, strerror(err));
-            close(sock_fd);
+            LOG_ERROR("failed to bind the UNIX domain socket to %s, ret: %s", socket_path, strerror(ret));
+            close(socket_fd);
             return ret;
         }
     }
 
-    if (listen(sock_fd, 5) != 0) {
+    if (listen(socket_fd, 5) != 0) {
         ret = errno;
-        LOG_ERROR("failed to listen to socket: %s", strerror(err));
-        close(sock_fd);
+        LOG_ERROR("failed to listen to socket: %s", strerror(ret));
+        close(socket_fd);
         return ret;
     }
-    *fd = sock_fd;
+    *fd = socket_fd;
     return ret;
 }
 
@@ -134,27 +146,26 @@ void *msg_poll(void *arg)
 
     cli_handler_t *handler = (cli_handler_t *)arg;
     while (true) {
-        struct pollfd fds[2];
+        struct pollfd fds[1];
         // FIPS zeroization audit 20191115: this memset is fine.
         memset(fds, 0, sizeof(fds));
         fds[0].fd = handler->socket_fd;
         fds[0].events = POLLIN | POLLRDBAND;
 
         LOG_DEBUG("entry waiting");
-        int ret = poll(fds, 2, -1);
+        int ret = poll(fds, 1, -1);
         if (ret < 0) {
-            int err = ceph_sock_errno();
-            if (err == EINTR) {
+            if (ret == EINTR) {
                 continue;
             }
-            LOG_ERROR("poll(2) error: %s", strerror(err));
-            return;
+            LOG_ERROR("poll(2) error: %s", strerror(ret));
+            return NULL;
         }
         LOG_DEBUG("entry wake");
 
         if (fds[0].revents & POLLIN) {
             // Send out some data
-            do_accept();
+            do_accept(fds[0].fd);
         }
     }
     return NULL;
@@ -251,13 +262,13 @@ char *comand_output()
 }
 
 
-comamnd_t *command_find(const char *subcommand)
+command_t *command_find(const char *subcommand)
 {
     for (int32_t i = 0; i < 1024; i++) {
         if (commands[i].subcommand == NULL) {
             return NULL;
         }
-        if (strncmp(commands[i].subcommand, subcommand, CMD_LEN) == 0) {
+        if (strncmp(commands[i].subcommand, subcommand, COMMAND_MAX_LEN) == 0) {
             return &commands[i];
         }
     }
@@ -268,7 +279,7 @@ int32_t command_execute(const char*subcommand, int32_t argc, char **argv)
 {
     command_t *cmd = command_find(subcommand);
     if (cmd) {
-        cmd.handler(argc, argv);
+        cmd->handler(argc, argv);
     }
     return 0;
 }
@@ -310,7 +321,7 @@ void do_accept(int32_t socket_fd)
     char *argv[32] = {0};
     msg_decode(cmd, subcommand, &argc, argv);
     
-    int rval = command_execute(msg.subcommand, argc, argv);
+    int rval = command_execute(subcommand, argc, argv);
 
 
     char *output = comand_output();
